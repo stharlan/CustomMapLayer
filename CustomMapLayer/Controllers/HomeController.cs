4ad8845179b750c5b72ec5c0164c8f70c78af097
Stuart Harlan <stuart.harlan@outlook.com>
2018-04-21 18:13:50 -0500

    initial commit

---
+++ CustomMapLayer/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SkiaSharp;
+using System.IO;
+
+namespace CustomMapLayer.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Layer1(int width, int height)
+        {
+            return new FileGeneratingResult("map.csv", "text/csv",
+                stream => this.GenerateExportFile(width, height, stream));
+        }
+
+        public void GenerateExportFile(int width, int height, Stream stream)
+        {
+            SKImageInfo info = new SKImageInfo(width, height, SKColorType.Bgra8888, SKAlphaType.Premul);
+            using (SKBitmap bmp = new SKBitmap(info))
+            {
+                using (SKCanvas canvas = new SKCanvas(bmp))
+                {
+                    canvas.Clear(SKColors.Transparent);
+                    using (SKPaint paint = new SKPaint())
+                    {
+                        paint.IsAntialias = true;
+                        paint.Color = new SKColor(255, 0, 0, 127);
+                        canvas.DrawCircle(width / 2, height / 2, 100, paint);
+                    }
+                }
+                using (SKImage img = SKImage.FromBitmap(bmp))
+                using (SKData data = img.Encode(SKEncodedImageFormat.Png, 100))
+                {
+                    data.SaveTo(stream);
+                }
+            }
+        }
+    }
+}
---
+++ skiatest/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SkiaSharp;
+
+namespace skiatest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SKImageInfo info = new SKImageInfo(640, 480, SKColorType.Bgra8888, SKAlphaType.Premul);
+            using (SKBitmap bmp = new SKBitmap(info))
+            {
+                using (SKCanvas canvas = new SKCanvas(bmp))
+                {
+                    canvas.Clear(SKColors.Transparent);
+                    using (SKPaint paint = new SKPaint())
+                    {
+                        paint.IsAntialias = true;
+                        paint.Color = SKColors.Red;
+                        canvas.DrawCircle(100, 100, 100, paint);
+                    }
+                }
+                using (SKImage img = SKImage.FromBitmap(bmp))
+                using (SKData data = img.Encode(SKEncodedImageFormat.Png, 100))
+                using (System.IO.FileStream fout = System.IO.File.OpenWrite("test.png"))
+                {
+                    data.SaveTo(fout);
+                }
+            }
+            using (SKBitmap bmp = new SKBitmap(info))
+            {
+                using (SKCanvas canvas = new SKCanvas(bmp))
+                {
+                    canvas.Clear(SKColors.Transparent);
+                    using (SKPaint paint = new SKPaint())
+                    {
+                        paint.IsAntialias = true;
+                        paint.Color = SKColors.Blue;
+                        canvas.DrawRect(50, 50, 200, 200, paint);
+                    }
+                }
+                using (SKImage img = SKImage.FromBitmap(bmp))
+                using (SKData data = img.Encode(SKEncodedImageFormat.Png, 100))
+                using (System.IO.FileStream fout = System.IO.File.OpenWrite("test1.png"))
+                {
+                    data.SaveTo(fout);
+                }
+            }
+        }
+    }
+}
